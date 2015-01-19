ef7396cf6ca3655d4b2bb13bdef9ae7a2a545d1d
shock <github@wdoughty.net>
2015-01-19 00:44:44 -0600

    destroy oldest db backup from heroku

--- script/dump_database.sh
+++ script/dump_database.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+oldest_backup_id=$(heroku pgbackups | grep DATABASE_URL | awk '{print $1}' | tail -r | tail -1)
+echo "destroying oldbest backup id $oldest_backup_id"
+heroku pgbackups:destroy $oldest_backup_id
 heroku pgbackups:capture
 curl -o tmp/latest.dump `heroku pgbackups:url`
