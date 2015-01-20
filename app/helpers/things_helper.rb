318b3a609b8013f6213d90693234ad865355c01f
shock <github@wdoughty.net>
2015-01-19 19:04:51 -0600

    Fix tree action click handler quirks

--- app/helpers/things_helper.rb
+++ app/helpers/things_helper.rb
@@ -17,23 +17,25 @@ module ThingsHelper
   def thing_actions(thing)
     id = thing.id
     %Q{
-      <em class="thing-actions action">
-        <a title="Edit #{thing.name}" class="a-edit-thing" href="/things/#{id}/edit">
-          <i class="fa fa-pencil-square default"></i>
-        </a>
-        <a title="Add Contained Thing" class="a-new-thing" href="/things/new?parent_id=#{id}">
-          <i class="fa fa-cube primary"></i>
-        </a>
-        <a title="Toggle Marked" class="#{id}_marked" data-remote="true" href="/things/#{id}/toggle_marked">
-          <i class="fa fa-check-square dimmed #{"marked" if thing.marked}"></i>
-        </a>
-      </em>
-      <em class="show-actions action">
-        <a href="#"><i class="fa fa-bars"></i></a>
-        <a title="Toggle Marked" class="#{id}_marked" data-remote="true" href="/things/#{id}/toggle_marked">
-          <i class="fa fa-check-square dimmed #{"marked" if thing.marked}"></i>
-        </a>
-      </em>
+      <b class="action">
+        <em class="thing-actions">
+          <a title="Edit #{thing.name}" class="a-edit-thing" href="/things/#{id}/edit">
+            <i class="fa fa-pencil-square default"></i>
+          </a>
+          <a title="Add Contained Thing" class="a-new-thing" href="/things/new?parent_id=#{id}">
+            <i class="fa fa-cube primary"></i>
+          </a>
+          <a title="Toggle Marked" class="#{id}_marked" data-remote="true" href="/things/#{id}/toggle_marked">
+            <i class="fa fa-check-square dimmed #{"marked" if thing.marked}"></i>
+          </a>
+        </em>
+        <em class="show-actions">
+          <a href="#"><i class="fa fa-bars"></i></a>
+          <a title="Toggle Marked" class="#{id}_marked" data-remote="true" href="/things/#{id}/toggle_marked">
+            <i class="fa fa-check-square dimmed #{"marked" if thing.marked}"></i>
+          </a>
+        </em>
+      </b>
     }
   end
 
